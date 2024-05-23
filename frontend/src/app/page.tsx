bbe9bd54fbfbadaa2929b3c01c6df1f0ac79b42a
Utsav Lal <ualal@ncsu.edu>
2024-05-22 23:24:11 -0400

    frontend changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,39 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Welcome to shopping app</div>
-    </main>
-  );
+import Navbar from "@/app/components/navbar";
+
+const API_BASE_URL = "http://localhost:8080/api";
+const API_VERSION = "v1";
+
+export async function getProducts() {
+    console.log(`${API_BASE_URL}/${API_VERSION}/products}`);
+    const res = await fetch(`${API_BASE_URL}/${API_VERSION}/products`, {
+        method: "GET",
+    });
+
+    if (!res.ok) {
+        throw new Error(`${res.status}`);
+    }
+    return res.json();
+}
+
+interface Product {
+    id: number,
+    name: string,
+    description: string,
+    image: string,
+    price: number,
+    quantity: number,
+    created_at: Date,
+}
+
+export default async function Page() {
+    const products: Product[] = await getProducts();
+    return (
+        <main>
+            <Navbar/>
+            <div className="flex min-h-screen flex-col items-center p-24">
+                <div>Welcome to shopping app</div>
+                <div>{products.map((product) => product.name)} </div>
+            </div>
+        </main>
+    );
 }
--- frontend/src/app/someroute/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <h1>Hello from route</h1>;
-}
